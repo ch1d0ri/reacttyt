6ad8785ace665d0f97d0dd54362901eadd4535e2
ch1d0ri <remoxgolem@gmail.com>
2022-10-02 21:39:37 +0300

    frist commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const  Header = () =>{
+    return(
+        <header className="header">
+        <img src="https://www.pngmart.com/files/17/Cartoon-Rocket-Raccoon-PNG-Clipart.png"></img>
+      </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const  Profile = () =>{
+    return(
+      <div className="content">
+        <div>
+          <img src="https://www.thoughtco.com/thmb/BPfVCu8UcXZ5rFLYILOIpR251d8=/1500x844/smart/filters:no_upscale()/taiga-57bf24215f9b5855e5f3dd09.jpg"></img>
+        </div>
+        <div>
+          ava + descripstion
+        </div>
+        <div>
+          My posts
+          <div>
+            new posts
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+export default Profile;
